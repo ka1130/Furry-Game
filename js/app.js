f19a45d0925c2c911b99d78bc96b739222db8fc9
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-05-13 12:28:19 +0200

    Draw Furry and Coin

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@
   document.addEventListener("DOMContentLoaded", function (event) {
 
     var Furry = function () {
-      this.x;
-      this.y;
-      this.dir;
+      this.x = 0;
+      this.y = 0;
+      this.dir = "right";
     };
 
     var Coin = function () {
@@ -13,15 +13,30 @@
       this.y = Math.floor(Math.random() * 10);
     };
 
-    var Game = function (board, furry, coin, score) {
+    var Game = function () {
       this.board = document.getElementById("board").children;
       this.furry = new Furry();
       this.coin = new Coin();
       this.score = 0;
     };
 
+    Game.prototype.getIndex = function (x, y) {
+      console.log(x + y * 10);
+      return x + y * 10;
+    };
 
-    console.log(Game);
+    Game.prototype.drawGame = function () {
+      var furryPos = this.getIndex(this.furry.x, this.furry.y);
+      var coinPos = this.getIndex(this.coin.x, this.coin.y);
+
+      this.board[furryPos].classList.add("furry");
+      this.board[coinPos].classList.add("coin");
+    };
+
+    var myGame = new Game();
+    myGame.drawGame();
+
+    console.log(myGame);
 
 
     //end
